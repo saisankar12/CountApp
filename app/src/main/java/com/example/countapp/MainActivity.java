99ebdf91025474097296428535eeea6217d22a94
saisankar12 <saisankar75@gmail.com>
2019-11-01 12:10:47 +0530

    Update UI & Java Functionality

--- app/src/main/java/com/example/countapp/MainActivity.java
+++ app/src/main/java/com/example/countapp/MainActivity.java
@@ -10,29 +10,32 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button b1;
-    int a=0;
+    int a = 0;
     TextView tv;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        b1=findViewById(R.id.count_inc);
-        tv=findViewById(R.id.text_count);
+        b1 = findViewById(R.id.count_inc);
+        tv = findViewById(R.id.text_count);
 
-        if (savedInstanceState!=null){
-            String s=savedInstanceState.getString("number");
-            tv.setText(s);
-        }else {
-            b1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    a++;
-                    tv.setText(""+a);
-                }
-            });
-
+        if (savedInstanceState != null) {
+            String s = savedInstanceState.getString("number");
+            if (tv!=null){
+                tv.setText(s);
+            }
         }
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                a++;
+                tv.setText("" + a);
+            }
+        });
     }
+
     public void toast(View view) {
         Toast.makeText(this, "Android",
                 Toast.LENGTH_SHORT).show();
@@ -42,6 +45,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
 
-        outState.putString("number",tv.getText().toString());
+        outState.putString("number", String.valueOf(tv.getText()));
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        if (savedInstanceState != null) {
+            String s = savedInstanceState.getString("number");
+            if (tv!=null){
+                tv.setText(s);
+            }
+        }
+
     }
 }
